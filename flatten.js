e45221aa3764e1b2d7c4db5c537028df327ffcb7
Matthew Thompson <supra__charged@hotmail.com>
2021-04-28 03:47:25 +0000

    Add flatten.js

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      continue;
+    }
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const flatten = function(nestedArray) {
+  const result = [];
+  const flatHandler = function(array) {
+    for (let item of array) {
+      if (Array.isArray(item)) {
+        flatHandler(item);
+      } else {
+        result.push(item);
+      }
+    }
+  };
+  flatHandler(nestedArray);
+  return result;
+};
+
+assertArraysEqual(flatten([1, [2, 3, [4, [5, 6], 7], 8], [9, 10]]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
