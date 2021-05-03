1e12945e6c6b96ca8269db7a6ee499c810e724e4
Matthew Thompson <supra__charged@hotmail.com>
2021-05-03 20:12:21 +0000

    reqrite eqArrayTest tests using mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,20 +1,36 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false);
-assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
-assertEqual(eqArrays([], []), true);
+describe('eqArrays', () => {
+  it('it should return true if arrays with equal primitive values are passed in', () => {
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
+  });
 
-// Arrays of arrays
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-assertEqual(eqArrays([[2, 3], [4, 5, [7, 8]]], [[2, 3], [4, 5, [7, 8]]]), true);
-assertEqual(eqArrays(
-  [[2, 3], [3, 3, [2], [2, [22, [19, [[[8]]]]]]], [4, 5, [7, 8, [4, 4], 7, [8, [9]]]]],
-  [[2, 3], [4, 5, [7, 8]]]
-), false);
-assertEqual(eqArrays(
-  [[2, 3], [3, 3, [2], [2, [22, [19, [[[8]]]]]]], [4, 5, [7, 8, [4, 4], 7, [8, [9]]]]],
-  [[2, 3], [3, 3, [2], [2, [22, [19, [[[8]]]]]]], [4, 5, [7, 8, [4, 4], 7, [8, [9]]]]]
-), true);
+  it('it should return false if arrays with unequal primitive values are passed in', () => {
+    assert.isFalse(eqArrays([1, 2, 3], [1, 2, '3']));
+  });
+
+  it('it should return false if arrays of different lengths are passed in', () => {
+    assert.isFalse(eqArrays([1, 2], [1, 2, 3]));
+  });
+
+  it('should return true if two empty arrays are passed in', () => {
+    assert.isTrue(eqArrays([], []));
+  });
+
+  it('should return true for equal arrays of arrays', () => {
+    assert.isTrue(eqArrays([[2, 3], [4, 5, [7, 8]]], [[2, 3], [4, 5, [7, 8]]]));
+  });
+
+  it('should return true for deeply nested equal arrays of arrays of primitives', () => {
+    assert.isTrue(eqArrays(
+      [[2, 3], [3, 3, [2], [2, [22, [19, [[[8]]]]]]], [4, 5, [7, 8, [4, 4], 7, [8, [9]]]]],
+      [[2, 3], [3, 3, [2], [2, [22, [19, [[[8]]]]]]], [4, 5, [7, 8, [4, 4], 7, [8, [9]]]]]
+    ));
+  });
+
+  it('should return false for unequal arrays of arrays', () => {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]));
+  });
+});
+
