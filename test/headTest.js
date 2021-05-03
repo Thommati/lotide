43b0e21b18e1aca7e93e562e636a66b6020db327
Matthew Thompson <supra__charged@hotmail.com>
2021-05-03 19:34:54 +0000

    Refactor headTest.js to use mocha chai tests

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,12 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Cat', 'Dog', 'Baby']), 'Cat');
-assertEqual(head(['Cat']), 'Cat');
-assertEqual(head([]), undefined);
-assertEqual(head(null), undefined);
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it('returns "5" for  ["5"]', () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
