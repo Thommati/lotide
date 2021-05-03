839e1c9ebe6b41ed542a785cd1a8997bc05de137
Matthew Thompson <supra__charged@hotmail.com>
2021-05-03 20:29:39 +0000

    Edit tests in tailTest to use deepEqual

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,25 +2,19 @@ const assert = require('chai').assert;
 const tail = require('../tail');
 
 describe('#tail', () => {
-  it('should return an array with a first element of "Dog"', () => {
-    assert.strictEqual(tail(['Cat', 'Dog', 'Baby'])[0], 'Dog');
-  });
-
-  it('should return and array with a second element of "Baby"', () => {
-    assert.strictEqual(tail(['Cat', 'Dog', 'Baby'])[1], 'Baby');
-  });
-
-  it('should return an array of length zero when passed an array of length one', () => {
-    assert.strictEqual(tail(['Cat']).length, 0);
+  it('returns ["Dog", "Baby"] when passed ["Cat", "Dog", "Baby"]', () => {
+    assert.deepEqual(tail(['Cat', 'Dog', 'Baby']), ['Dog', 'Baby']);
   });
 
   it('should return an array of length zero when passed an empty array', () => {
-    assert.strictEqual(tail([]).length, 0);
+    const result = tail([]);
+    assert.isArray(result);
+    assert.strictEqual(result.length, 0);
   });
 
-  it('should not modify the length of the input array', () => {
-    const input = ['Cat', 'Dog', 'Baby'];
-    tail(input);
-    assert.strictEqual(input.length, 3);
+  it('should not modify the input array', () => {
+    const originalArray = ['Cat', 'Dog', 'Baby'];
+    tail(originalArray);
+    assert.deepEqual(originalArray, ['Cat', 'Dog', 'Baby']);
   });
 });
