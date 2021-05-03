9d2d36df7f69f6353567a11ec4a800f9e7061664
Matthew Thompson <supra__charged@hotmail.com>
2021-05-03 19:51:07 +0000

    Remove unnecessary comments from tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -24,17 +24,3 @@ describe('#tail', () => {
     assert.strictEqual(input.length, 3);
   });
 });
-
-// const assertEqual = require('../assertEqual');
-
-// const arr = ['Cat', 'Dog', 'Baby'];
-// const actual = tail(arr);
-// assertEqual(actual[0], 'Dog');
-// assertEqual(actual[1], 'Baby');
-
-// assertEqual(tail(['Cat']).length, 0);
-// assertEqual(tail([]).length, 0);
-
-// // Check that the original array was not modified.
-// assertEqual(arr.length, 3);
-// assertEqual(actual.length, 2);
